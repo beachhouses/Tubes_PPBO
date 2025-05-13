530b5c10766779f7fc1e47db3009f2e994f1335c
Syarifah Chadijah <syarifah.chadijah06@gmail.com>
2025-05-13 19:39:31 +0700

    Add files via upload

--- menu.cpp
+++ menu.cpp
@@ -15,7 +15,7 @@ string MoodBites::getNamaCustomer() {
 }
 
 void MoodBites::tampilkanJudul() {
-    cout << MERAH;
+    cout << MAGENTA;
     cout << "############################################\n";
     cout << "#                                          #\n";
     cout << "#           WELCOME TO MOODBITES!          #\n";
@@ -25,7 +25,7 @@ void MoodBites::tampilkanJudul() {
 }
 
 void MoodBites::pilihMood() {
-    cout << MERAH;
+    cout << MAGENTA;
     cout << "\nHai " << namaCustomer << ", mood kamu hari ini gimana nih?\n";
     cout << KUNING;
     cout << "+------------------------+\n";
@@ -40,20 +40,22 @@ void MoodBites::pilihMood() {
     cin >> mood;
 
     if (mood == 1) {
-        cout << MERAH << "Yaah, kamu lagi sedih ya? Kami punya menu yang bisa bikin kamu lebih baik!\n" << RESET;
+        cout << CYAN << "Yaah, kamu lagi sedih ya? Kami punya menu yang bisa bikin kamu lebih baik!\n" << RESET;
     } else if (mood == 2) {
-        cout << MERAH << "Ciee lagi senang ya? Yuk coba menu-menu yang bikin mood kamu makin top!\n" << RESET;
+        cout << PINK << "Ciee lagi senang ya? Yuk coba menu-menu yang bikin mood kamu makin top!\n" << RESET;
     } else if (mood == 3) {
-        cout << MERAH << "Gak apa-apa kok kalo lagi marah, coba deh menu-menu ini!\n" << RESET;
+        cout << MERAH << "Gak apa-apa kok kalo lagi marah, tenangin diri dan coba deh menu-menu ini!\n" << RESET;
     } else if (mood == 4) {
-        cout << MERAH << "Mungkin kamu lagi biasa aja, tapi ada menu yang pasti bikin kamu puas!\n" << RESET;
+        cout << HIJAU << "Mungkin kamu lagi biasa aja, tapi ada menu yang pasti bikin kamu jadi luar biasa!\n" << RESET;
     }
 }
 
 void MoodBites::tampilkanMenu() {
     string menu[5];
     int hargaMenu[5];
+    string warnaTabel;
 
+    // Daftar menu dan harga sesuai mood
     if (mood == 1) {
         menu[0] = "Es Krim Coklat"; hargaMenu[0] = 15000;
         menu[1] = "Brownies Lumer"; hargaMenu[1] = 18000;
@@ -80,11 +82,17 @@ void MoodBites::tampilkanMenu() {
         menu[4] = "Mie Yamin Pedas Manis"; hargaMenu[4] = 20000;
     }
 
-    cout << MERAH << "\nIni nih menu yang sesuai dengan mood kamu!\n";
-    cout << KUNING << "+=======================================================+\n";
+    if (mood == 1) warnaTabel = CYAN;
+    else if (mood == 2) warnaTabel = PINK;
+    else if (mood == 3) warnaTabel = MERAH;
+    else if (mood == 4) warnaTabel = HIJAU;
+
+    cout << MAGENTA << "\nIni nih menu yang sesuai dengan mood kamu!\n";
+    cout << warnaTabel << "+=======================================================+\n";
     for (int i = 0; i < 5; ++i) {
-        cout << "| " << i+1 << ". " << setw(30) << left << menu[i]
+        cout << warnaTabel
+            << "| " << i+1 << ". " << setw(30) << left << menu[i]
             << " | Harga: Rp" << setw(7) << hargaMenu[i] << " |\n";
     }
-    cout << "+=======================================================+\n" << RESET;
+    cout << warnaTabel << "+=======================================================+\n" << RESET;
 }
